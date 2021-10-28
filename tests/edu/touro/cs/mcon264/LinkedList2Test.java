f7ff3bc3b9b17c974bb36e4822da7b878ab594d8
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-10-28 16:31:21 -0400

    Initial commit

---
+++ tests/edu/touro/cs/mcon264/LinkedList2Test.java
@@ -0,0 +1,38 @@
+package edu.touro.cs.mcon264;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedList2Test {
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void set() {
+    }
+}
