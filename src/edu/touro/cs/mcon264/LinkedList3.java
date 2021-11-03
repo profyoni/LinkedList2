e40a141ad1c91365b6e6b2d46c1b022b0b6a3736
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-11-03 16:53:46 -0400

    rename

--- src/edu/touro/cs/mcon264/LinkedList3.java
+++ src/edu/touro/cs/mcon264/LinkedList3.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class LinkedList2<T> implements List<T> {
+public class LinkedList3<T> implements List<T> {
 
     private Node<T> head = new Node<>(null,null);
     private int size;
